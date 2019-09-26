16f23214458c29121b788ffe03908277becef271
liguobingh <2437053693@qq.com>
2019-09-26 11:18:35 +0800

    整理旧文档

---
+++ 空调/上报格式.java
@@ -0,0 +1,154 @@
+{
+	"method": "props",
+	"params": {
+		"power": 1
+	}
+}
+
+return "ok"
+return "error"
+"invalid cmd" -300
+
+[{
+	"key": "prop.power",
+	"time": 1545645471,
+	"value": [1]
+}]
+
+=============================
+
+{
+	"method": "props",
+	"params": {
+		"temperature": 25
+	}
+}
+
+return "ok"
+return "error"
+"invalid cmd" -300
+
+[{
+	"key": "prop.temperature",
+	"time": 1545645471,
+	"value": [-12]
+}]
+
+=============================
+
+{
+	"method": "props",
+	"params": {
+		"mode": 2
+	}
+}
+
+return "ok"
+return "error"
+"invalid cmd" -300
+
+[{
+	"key": "prop.mode",
+	"time": 1545645471,
+	"value": [2]
+}]
+
+=============================
+
+{
+	"method": "props",
+	"params": {
+		"wind_level": 2
+	}
+}
+
+return "ok"
+return "error"
+"invalid cmd" -300
+
+[{
+	"key": "prop.wind_level",
+	"time": 1545645471,
+	"value": [2]
+}]
+
+=============================
+
+{
+	"method": "props",
+	"prams": {
+		"wind_percent": 80
+	}
+}
+
+return "ok"
+return "error"
+"invalid cmd" -300
+
+[{
+	"key": "prop.wind_percent",
+	"time": 1545645471,
+	"value": [80]
+}]
+
+=============================
+
+{
+	"method":"props",
+		"params"{
+		"swing"
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
